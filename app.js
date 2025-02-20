d81c65c64efb61198fe71b55248de0bd38e4dc47
Afshan kanwal <kanwalafshan2244@gmail.com>
2025-02-21 01:03:20 +0500

    assignment5

---
+++ app.js
@@ -0,0 +1,37 @@
+//Qno:1 Write a program that takes two user inputs for first and last name using prompt and merge them in a new variable titled fullName. Greet the user using his full name.
+
+// let firstName =prompt("ENTER YOUR FIRST NAME");
+// let lastName= prompt("ENTER YOUR LAST NAME");
+// let fullName=firstName+lastName;
+// alert(`${fullName}`);
+
+// Qno:2
+// let myFavoritePhone=" Samsung Galaxy S6 Edge Plus"
+// console.log(myFavoritePhone.length)
+// document.write(`My Favorite Mobile Phone is: ${myFavoritePhone}<br />`)
+// document.write(`Length  of String: ${myFavoritePhone.length}`);
+
+// Qno:3
+// let countryName= "Pakistani"
+// console.log(countryName.indexOf("n"));
+// document.write(`String:${countryName}<br />`)
+// document.write(`Index of 'n': ${countryName.indexOf("n")}`)
+
+// Qno:4
+// let wordName="Hello World"
+// console.log(wordName.lastIndexOf("l"));
+// document.write(`String: ${wordName}<br />`);
+// document.write(`Last Index of 'l': ${wordName.lastIndexOf("l")} `);
+
+// Qno:5
+// let character="Pakistani"
+// console.log(character.slice(3,4));
+// document.write(`String:${character}<br />`);
+// document.write(`Character at Index ${character.indexOf("i")}:${character.slice(3,4)}`);
+
+// Qno:6
+
+
+
+
+
